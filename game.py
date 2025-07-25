8aae68273040ac0866e772f31eea6b81f01f4c05
Bazinga9000 <bazonkidonk@gmail.com>
2025-07-24 20:36:45 -0400

    proper subclassing of views

--- game.py
+++ game.py
@@ -56,80 +56,8 @@ class Game:
 
         e.add_field(name="Top Card", value=self.piles[0][-1].display_name)
 
-        v = discord.ui.View()
+        await self.channel.send(embed = e, view = TurnStarterView(self))
 
-        async def turn_callback(interaction):
-            if interaction.user != active_player:
-                return await interaction.respond("It's not your turn!", ephemeral=True)
-
-            await interaction.respond(view=self.card_play_view(active_player), ephemeral=True)
-
-
-        turn_button = discord.ui.Button(label=f"Take Turn ({active_player.display_name})", row=1, style=discord.ButtonStyle.blurple)
-        turn_button.callback = turn_callback
-        v.add_item(turn_button)
-
-        await self.channel.send(embed = e, view = v)
-
-    # Generates the view that allows a player to play a card
-    def card_play_view(self, player, can_pass=False):
-        v = discord.ui.View()
-
-        playable = []
-        for cn,c in enumerate(self.hands[player]):
-            for pn in range(len(self.piles)):
-                if c.can_play(self, pn):
-                    playable.append(discord.SelectOption(
-                        label = f"{c.display_name} onto pile #{pn+1}",
-                        value = f"{cn},{pn}" # Values can only be strings, so this is easily parseable int,int
-                    ))
-
-        if len(playable) == 0:
-            # No need to add the modal/button for playing a card, no cards to play
-            draw_button = discord.ui.Button(label = "Draw a card", style = discord.ButtonStyle.danger)
-            async def draw_callback(interaction):
-                self.hands[player].append(self.deck.pop())
-                await self.channel.send(f"{player.display_name} drew a card!")
-                await interaction.edit(view=self.card_play_view(player, can_pass=True))
-
-            draw_button.callback = draw_callback
-
-            pass_button = discord.ui.Button(label = "Pass turn", style = discord.ButtonStyle.blurple)
-            async def pass_callback(interaction):
-                await self.channel.send(f"{player.display_name} passed their turn!")
-                await interaction.response.defer()
-                await interaction.delete_original_response()
-                await self.end_turn()
-            pass_button.callback = pass_callback
-
-            v.add_item(draw_button)
-            if can_pass: v.add_item(pass_button)
-
-            return v
-
-        s = discord.ui.Select(
-            placeholder = "Select a card to play...",
-            min_values = 1,
-            max_values = 1,
-            options = playable
-        )
-
-        async def select_callback(interaction):
-            chosen = s.values[0].split(",") # pyright: ignore (split is safe, all values will be strings)
-            chosen_card_idx = int(chosen[0])
-            chosen_pile = int(chosen[1])
-
-            chosen_card = self.hands[player].pop(chosen_card_idx)
-            self.piles[chosen_pile].append(chosen_card)
-
-            await interaction.response.defer()
-            await interaction.delete_original_response()
-            await self.end_turn()
-
-        s.callback = select_callback
-        v.add_item(s)
-
-        return v
 
     # Performs cleanup at the end of a turn and then starts the next turn if needed
     async def end_turn(self):
@@ -142,3 +70,159 @@ class Game:
             self.whose_turn = (self.whose_turn + 1) % len(self.players)
             self.turn += 1
             await self.start_turn()
+
+
+# A view that can keep track of the game's turns, and disable callbacks if they're out of turn
+class TurnTrackingView(discord.ui.View):
+    def __init__(self, game: Game):
+        super().__init__()
+        self.game = game
+        self.turn = game.turn
+        self.player = game.players[game.whose_turn]
+
+    # Wrap a callback to validate the turn
+    # The actual callbacks are thus assumed to be correct
+
+    # Delete the entire message if the interaction is done out of turn
+    def delete_out_of_turn(self, cb):
+        async def wrapper(interaction):
+            if self.game.turn > self.turn:
+                # This view is no longer required. Kill the message!
+                await interaction.response.defer()
+                await interaction.delete_original_response()
+                self.stop()
+                return
+            await cb(interaction)
+        return wrapper
+
+    # Empty the view if the interaction is done out of turn, but do not delete the message
+    def empty_out_of_turn(self, cb):
+        async def wrapper(interaction):
+            if self.game.turn > self.turn:
+                # This view is no longer required. Remove it from the message!
+                await interaction.edit(view=None)
+                self.stop()
+                return
+            await cb(interaction)
+        return wrapper
+
+class TurnStarterView(TurnTrackingView):
+    def __init__(self, game: Game):
+        super().__init__(game)
+        turn_button = discord.ui.Button(label=f"Take Turn ({self.player.display_name})", row=1, style=discord.ButtonStyle.blurple)
+        turn_button.callback = self.empty_out_of_turn(self.turn_callback)
+        self.add_item(turn_button)
+
+
+    async def turn_callback(self, interaction):
+        if interaction.user != self.player:
+            return await interaction.respond("It's not your turn!", ephemeral=True)
+
+        await interaction.respond(view=ActivePlayerView(self.game), ephemeral=True)
+
+# The view shown to the active player in an ephemeral message to play cards/choose targets/etc
+class ActivePlayerView(TurnTrackingView):
+    def __init__(self, game: Game):
+        super().__init__(game)
+        # Can this player pass their turn?
+        self.can_pass = False
+
+        # Which card does the player want to play?
+        self.selected_card_index = None
+        # Where do they want to play it?
+        self.selected_pile = None
+
+        # Set up the core buttons
+        # Draw a card
+        self.draw_button = discord.ui.Button(label = "Draw a card", style = discord.ButtonStyle.danger)
+        self.draw_button.callback = self.delete_out_of_turn(self.draw_callback)
+        self.add_item(self.draw_button)
+        # Pass your turn
+        self.pass_button = discord.ui.Button(label = "Pass turn", style = discord.ButtonStyle.blurple)
+        self.pass_button.callback = self.delete_out_of_turn(self.pass_callback)
+        self.add_item(self.pass_button)
+        # Play a card
+        self.play_button = discord.ui.Button(label = "Play selected card", style = discord.ButtonStyle.success)
+        self.play_button.callback = self.delete_out_of_turn(self.play_callback)
+        self.add_item(self.play_button)
+        # Card selector (added in update_items if needed)
+        self.card_selector = None
+        # Update the core items, enabling/disabling if neeeded
+        self.update_items(True)
+
+
+
+    # Update self and update the interaction
+    async def update(self, interaction, refresh_selector):
+        self.update_items(refresh_selector)
+        await interaction.edit(view=self)
+
+    # Update the items of this view
+    def update_items(self, refresh_selector):
+        self.pass_button.disabled = not self.can_pass
+        self.play_button.disabled = not self.can_play()
+
+
+        # Evaluate how many cards in the player's hand can be played, and where, and generate the requisite select options
+        g = self.game
+        playable = []
+        for cn,c in enumerate(g.hands[self.player]):
+            for pn in range(len(g.piles)):
+                if c.can_play(g, pn):
+                    playable.append(discord.SelectOption(
+                        label = f"{c.display_name} onto pile #{pn+1}",
+                        value = f"{cn},{pn}" # Values can only be strings, so this is easily parseable int,int
+                    ))
+
+        if playable == []: # no playable cards, no need to mess with the select modal
+            return
+
+        if self.card_selector is None:
+            self.card_selector = discord.ui.Select(
+                placeholder = "Select a card to play...",
+                min_values = 1,
+                max_values = 1,
+                options = playable
+            )
+            self.card_selector.callback = self.delete_out_of_turn(self.card_select_callback)
+            self.add_item(self.card_selector)
+            return
+
+        if refresh_selector:
+            self.card_selector.options = playable
+
+    async def draw_callback(self, interaction):
+        g = self.game
+        g.hands[self.player].append(g.deck.pop())
+        await g.channel.send(f"{self.player.display_name} drew a card!")
+        self.can_pass = True
+        await self.update(interaction, True) # card was drawn, need to remake the card selector
+
+    async def pass_callback(self, interaction):
+        g = self.game
+        await g.channel.send(f"{self.player.display_name} passed their turn!")
+        await interaction.response.defer()
+        await interaction.delete_original_response()
+        self.stop()
+        await g.end_turn()
+
+
+    # Should the play button be enabled?
+    def can_play(self):
+        return self.selected_pile is not None and self.selected_card_index is not None
+        # TODO: if a card requires arguments, e.g player target, wild color, check if they're set properly
+
+    async def play_callback(self, interaction):
+        g = self.game
+
+        chosen_card = g.hands[self.player].pop(self.selected_card_index)
+        g.piles[self.selected_pile].append(chosen_card) # pyright: ignore (this is safe, selected_pile will be non-None at this point)
+        await interaction.response.defer()
+        await interaction.delete_original_response()
+        await g.end_turn()
+
+    async def card_select_callback(self, interaction):
+        chosen = self.card_selector.values[0].split(",") # pyright: ignore (split is safe, all values will be strings)
+        self.selected_card_index = int(chosen[0])
+        self.selected_pile = int(chosen[1])
+        await self.update(interaction, False) # hand hasn't changed, no need to refresh the card selector's options
