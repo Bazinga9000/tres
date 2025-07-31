c8a249bb323bea8b6a986f79df7fdc566b509b87
Bazinga9000 <bazonkidonk@gmail.com>
2025-07-31 02:09:05 -0400

    skip cards are now named double/triple/etc skip

--- card/cards/skip_card.py
+++ card/cards/skip_card.py
@@ -2,6 +2,7 @@ from typing import override
 
 from card import Card
 from card.color import CardColor
+from util.number_names import tuple_name
 from . import Game
 from views.argbuilder import ArgBuilder
 
@@ -11,14 +12,14 @@ class SkipCard(Card):
         assert n >= 1
         super().__init__(color, 20, 0, f"skip_{n}", False)
         cn = self.color_name()
-        self.display_name = f"{cn} Skip {n}" if n > 1 else f"{cn} Skip"
+        self.display_name = f"{cn} {tuple_name(n)} Skip" if n > 1 else f"{cn} Skip"
         self.n = n
-    
+
     @property
     @override
     def args(self):
         return ArgBuilder().with_callback(self.on_play)
-    
+
     def on_play(self, game: Game):
         game.whose_turn += self.n
         game.whose_turn %= len(game.players)
---
+++ util/number_names.py
@@ -0,0 +1,20 @@
+def tuple_name(n: int):
+    '''
+    Return the name for an n-tuple of size n.
+    For values outside [1,10], defaults to n-fold.
+    '''
+    if 1 <= n <= 10:
+        return [
+            "Single",
+            "Double",
+            "Triple",
+            "Quadruple",
+            "Quintuple",
+            "Sextuple",
+            "Septuple",
+            "Octuple",
+            "Nonuple",
+            "Dectuple"
+            ][n-1]
+    else:
+        return f"{n}-fold"
