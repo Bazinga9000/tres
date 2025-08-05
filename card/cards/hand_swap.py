06c19559cb49e640194597968b97a0a2cba31813
Bazinga9000 <bazonkidonk@gmail.com>
2025-08-05 02:18:10 -0400

    remove unnecessary f strings

--- card/cards/hand_swap.py
+++ card/cards/hand_swap.py
@@ -8,12 +8,12 @@ from views.argbuilder import ArgBuilder
 
 class HandSwap(Card):
     def __init__(self, color: CardColor):
-        super().__init__(color, 30, 0, f"hand_swap", False)
-    
+        super().__init__(color, 30, 0, "hand_swap", False)
+
     @property
     @override
     def args(self):
         return ArgBuilder[Game]().add_player(skip_self=True).with_callback(self.on_play)
-    
+
     def on_play(self, game: Game, player: Player):
         player.hand, game.active_player.hand = game.active_player.hand, player.hand
